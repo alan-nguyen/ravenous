64ea5638e58118f66aa09ef8bddc6e94ca2c0be2
Alan Nguyen <tuan8101@gmail.com>
2020-04-22 21:57:28 -0600

    Create render method for SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,11 +7,28 @@ const sortByOptions = {
   'Most Reviewed': 'review_count',
 };
 
+// Dynamically create the list items needed to diplay
 class SearchBar extends React.Component {
   renderSortByOptions() {
     return Object.keys(sortByOptions).map((sortByOption) => {
       let sortByOptionValue = sortByOptions[sortByOption];
       return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
+  }
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{renderSortByOptions()}</ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+    );
   }
 }
