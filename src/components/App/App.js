8d5f30ff6cffcfe906a9f56e1355b09189fb263a
Alan Nguyen <tuan8101@gmail.com>
2020-04-25 22:58:15 -0600

    Add businesses props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,7 +22,7 @@ function App() {
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={this.businesses} />
     </div>
   );
 }
