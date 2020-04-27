1f5a9e37db979111356936f69dffd00619cbdc44
Alan Nguyen <tuan8101@gmail.com>
2020-04-27 16:58:28 -0600

    Add search function

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,4 +1,11 @@
+// Credential
 const apiKey =
   'i4Due_YmEmhrBbslgfYtw6OyWaDhtDYjs0R9m-ZIBthJGQAQkyMeTkjw05u7iV1GQ65B676iQt4_0fSbSJc3ICesfLnEcbGzcnTNn-kI1CL2htrPBbhdaW86K1enXnYx';
 
+  // Yelp Module
 const Yelp = {};
+
+const search(term, location, sortBy) {
+  let url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+  
+};
