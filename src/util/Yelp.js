ab7020f1d0c58dcdbba6b27f2c5023ba70654d1f
Alan Nguyen <tuan8101@gmail.com>
2020-04-27 17:07:58 -0600

    Add a key to Business

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,7 +2,7 @@
 const apiKey =
   'i4Due_YmEmhrBbslgfYtw6OyWaDhtDYjs0R9m-ZIBthJGQAQkyMeTkjw05u7iV1GQ65B676iQt4_0fSbSJc3ICesfLnEcbGzcnTNn-kI1CL2htrPBbhdaW86K1enXnYx';
 
-  // Yelp Module
+// Yelp Module
 const Yelp = {
   searchYelp(term, location, sortBy) {
     let url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
@@ -29,3 +29,5 @@ const Yelp = {
       });
   };
 };
+
+export default Yelp;
