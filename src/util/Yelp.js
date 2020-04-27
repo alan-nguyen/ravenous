3ef70709f472fe9a30379bac1a9fddb1704e64ec
Alan Nguyen <tuan8101@gmail.com>
2020-04-27 17:04:27 -0600

    Move search into Yelp Module

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,29 +3,29 @@ const apiKey =
   'i4Due_YmEmhrBbslgfYtw6OyWaDhtDYjs0R9m-ZIBthJGQAQkyMeTkjw05u7iV1GQ65B676iQt4_0fSbSJc3ICesfLnEcbGzcnTNn-kI1CL2htrPBbhdaW86K1enXnYx';
 
   // Yelp Module
-const Yelp = {};
-
-const search(term, location, sortBy) {
-  let url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
-  return fetch(url, {
-    headers: {Authorization: `Bearer ${apiKey}`}})
-    .then(response => response.json())
-    .then(jsonResponse => {
-      if(jsonResponse.businesses){
-        return jsonResponse.businesses.map(business => {
-          return {
-            id: business.id,
-            imgageSrc: business.imgageSrc,
-            name: business.name,
-            address: business.address,
-            city: business.city,
-            state: business.state,
-            zipCode: business.zipCode,
-            category: business.category,
-            rating: business.rating,
-            reviewCount: business.reviewCount
-          }
-        })
-      }
-    });
+const Yelp = {
+  searchYelp(term, location, sortBy) {
+    let url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+    return fetch(url, {
+      headers: {Authorization: `Bearer ${apiKey}`}})
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if(jsonResponse.businesses){
+          return jsonResponse.businesses.map(business => {
+            return {
+              id: business.id,
+              imgageSrc: business.imgageSrc,
+              name: business.name,
+              address: business.address,
+              city: business.city,
+              state: business.state,
+              zipCode: business.zipCode,
+              category: business.category,
+              rating: business.rating,
+              reviewCount: business.reviewCount
+            }
+          })
+        }
+      });
+  };
 };
